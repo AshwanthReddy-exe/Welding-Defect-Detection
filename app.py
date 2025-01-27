2a9905610bdaed2ce4e9d8d8766f842c54a43d62
Ashwanth Reddy <ashreddy06b@gmail.com>
2025-01-28 03:19:59 +0530

    Updated

--- app.py
+++ app.py
@@ -86,8 +86,7 @@ model.eval()
 # Define the image transform
 transform = transforms.Compose([
     transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.ToTensor()
 ])
 
 @app.route("/", methods=["GET"])
